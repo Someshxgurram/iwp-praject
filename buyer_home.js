edcbd0ed04b2f4be3a0f18bbef12b4db990303f9
Someshxgurram <someshgurram007@gmail.com>
2022-10-02 11:17:28 +0530

    update

---
+++ buyer_home.js
@@ -0,0 +1,33 @@
+function responsive(){
+    var y = document.getElementById("bars");
+    if (y.className === "fa fa-bars") {
+        y.className = "fa fa-close";
+    }
+    else {
+        y.className = "fa fa-bars";
+    }
+    var x=document.getElementById("menu");
+    if (x.className==="menu"){
+        x.className+=" res"
+    }
+    else{
+        x.className="menu"
+    }
+ 
+}
+function choice(){
+    var y = document.getElementById("choice");
+    if (y.className === "fa fa-caret-square-o-left") {
+        y.className = "fa fa-caret-square-o-right";
+    }
+    else {
+        y.className = "fa fa-caret-square-o-left";
+    }
+    var x=document.getElementById("choicem");
+    if (x.className==="choices"){
+        x.className+=" main"
+    }
+    else{
+        x.className="choices"
+    }
+}
