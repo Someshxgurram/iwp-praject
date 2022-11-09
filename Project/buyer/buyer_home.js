27a4c2e74358966e9afe30718a39d4cc5e1595b0
Somesh <88395681+Someshxgurram@users.noreply.github.com>
2022-11-09 06:25:56 +0530

    Update buyer_home.js

--- Project/buyer/buyer_home.js
+++ Project/buyer/buyer_home.js
@@ -35,12 +35,66 @@ function choice(){
 function con1(){
     a=document.getElementById('container1');
     b=document.getElementById('container2');
+    c=document.getElementById('container3');
+    d=document.getElementById('container4');
     if(a.className==="container"){
         a.className+=" close";
+        b.className="container";
+    }
+    else if(c.className==="container"){
+        c.className+=" close";
+        b.className="container";
+    }
+    else if(d.className==="container"){
+        d.className+=" close";
         b.className="container";
     }
     else{
         a.className="container";
         b.className+=" close";
+    }
+}
+function con2(){
+    a=document.getElementById('container1');
+    b=document.getElementById('container2');
+    c=document.getElementById('container3');
+    d=document.getElementById('container4');
+    if(a.className==="container"){
+        a.className+=" close";
+        c.className="container";
+    }
+    else if(b.className==="container"){
+        b.className+=" close";
+        c.className="container";
+    }
+    else if(d.className==="container"){
+        d.className+=" close";
+        c.className="container";
+    }
+    else{
+        a.className="container";
+        c.className+=" close";
+    }
+}
+function con3(){
+    a=document.getElementById('container1');
+    b=document.getElementById('container2');
+    c=document.getElementById('container3');
+    d=document.getElementById('container4');
+    if(a.className==="container"){
+        a.className+=" close";
+        d.className="container";
+    }
+    else if(b.className==="container"){
+        b.className+=" close";
+        d.className="container";
+    }
+    else if(c.className==="container"){
+        c.className+=" close";
+        d.className="container";
+    }
+    else{
+        a.className="container";
+        d.className+=" close";
     }
 }
